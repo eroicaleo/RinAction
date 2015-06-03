458e2a10d6c6e6a157e2c47dc39ef38fbc46dae8
Yang Ge <eroicaleo@gmail.com>
2015-06-03 15:56:49 -0700

    Updates Quiz1.R

--- GettingCleaningData/week1/Quiz1.R
+++ GettingCleaningData/week1/Quiz1.R
@@ -1,5 +1,4 @@
 # Problem 1
-
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv", destfile = "./ss06hid.csv", method = "curl")
 list.files("./")
 data <- read.csv("ss06hid.csv")
@@ -8,6 +7,10 @@ solution <- sum(data$VAL == 24, na.rm = TRUE)
 # Problem 2
 FES <- data$FES
 FES
+nameVec <- names(data)
+sum(nameVec == "FES")
+print("The problem with this variable is that it contains both marriage status and employment status.")
+print("We should have put them to 2 variables.")
 
 # Problem 3
 library(xlsx)
@@ -18,3 +21,23 @@ colIndex <- 7:15
 dat <- read.xlsx(file = "./gov_NGAP.xlsx", sheetIndex = 1, header = TRUE, rowIndex = rowIndex, colIndex = colIndex)
 dat
 sum(dat$Zip*dat$Ext,na.rm=T)
+
+# Problem 4
+library(XML)
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+download.file(fileUrl, destfile="./localFile.xml", method="curl", quiet=T)
+doc <- xmlTreeParse("./localFile.xml", useInternal=T)
+rootNode <- xmlRoot(doc)
+names(rootNode)
+rootNode[[1]][[1]]
+rootNode[[1]][[1]][[2]]
+zipcodeVec <- xpathSApply(doc = rootNode, "//zipcode", xmlValue)
+s <- length(zipcodeVec[zipcodeVec == "21231"])
+
+# Problem 5
+library(data.table)
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrl, destfile = "./06pid.csv", method = "curl")
+DT <- fread("./06pid.csv")
+head(DT)
+DT[,mean(pwgtp15),by=SEX]
